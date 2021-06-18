c2f4eeb7ca7992831958653039d4245868e375e2
Vincent Tran <vincentuong@yahoo.com>
2021-06-18 04:55:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,11 +13,13 @@ class Artist
   end
 
   def add_song(song)
+    @@song_count += 1
     @songs << song
     song.artist = self
   end
 
   def add_song_by_name(name)
+    @@song_count += 1
     song = Song.new(name)
     self.add_song(song)
     song
