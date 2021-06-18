4efb2ffbe5e2858e002812ffc39dc95ffc34d70f
Vincent Tran <vincentuong@yahoo.com>
2021-06-18 04:55:01 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,11 @@
 class Post
   
-  attr_accessor :name, :author
+  attr_accessor :title, :author
   
   @@all = []
   
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @@all << self
   end
   
